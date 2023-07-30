234364b2272a010a32a23e6f70d5dff663140305
Himanshu-code-wq <2000520320028@ietlucknow.ac.in>
2023-07-30 15:48:03 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirement(file_path:str)->List[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace("\n","")for req in requirement]
+
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+        return requirement
+
+setup(
+    name="cementStrength",
+    version="0.0.1",
+    author="TeamMarvel",
+    author_email="himanshugautam884@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirement('requirement.txt'),
+)
